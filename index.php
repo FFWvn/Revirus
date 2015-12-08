ad960e8fb895cd26b7d0a614ef52269398fd5832
HeoChauA <caothehai1991@gmail.com>
2015-12-08 14:20:26 +0700

    add sourcode

---
+++ index.php
@@ -0,0 +1,102 @@
+<!DOCTYPE html>
+<html>
+<head>
+  <title>Revirus</title>
+  <meta charset="utf-8">
+  <meta name="Description" content="">
+  <link rel="shortcut icon" href="/favicon.ico" type="image/x-icon">
+
+  <!-- Stylesheet -->
+  <link rel="stylesheet" href="css/style.css?v=1" type="text/css" media="screen" id="responsive">
+
+  <!-- Scripts -->
+  <script type="text/javascript" src="js/script.js?v=1"></script>
+
+
+  <!-- Import Font -->
+  <link href='https://fonts.googleapis.com/css?family=Open+Sans' rel='stylesheet' type='text/css'>
+  <link href='https://fonts.googleapis.com/css?family=Lato' rel='stylesheet' type='text/css'>
+
+</head>
+<body>
+  <div id="page">
+    <header class="header" id="header" role="banner">
+      <div class="row">
+        <div class="logo-header">
+          <a href="/" title="Revirus" rel="home" class="header__logo" id="logo">
+            <img src="logo.png" alt="Revirus" class="header__logo-image">
+          </a>
+        </div>
+
+        <div class="header__region region region-header">
+          <nav class="navigation" id="navigation">
+            <ul class="menu">
+              <li class="menu__item is-leaf first leaf"><a href="#" title="" class="menu__link">Om revirus</a></li>
+              <li class="menu__item is-leaf leaf"><a href="#" title="" class="menu__link">kurser</a></li>
+              <li class="menu__item is-leaf leaf"><a href="#" title="" class="menu__link">medlemmer</a></li>
+              <li  class="menu__item is-leaf last leaf"><a href="#" title="" class="menu__link">kontakt</a></li>
+            </ul>
+          </nav>
+
+          <div class="block block-banner-header">
+            <img src="images-template/revMand-lille.png" alt="banner">
+          </div>
+        </div>
+      </div>
+    </header>
+
+    <div id="main">
+      <div id="content" class="column" role="main">
+        <div class="element-invisible">
+          <h1 class="page__title title" id="page-title">Revirus</h1>
+        </div>
+
+        <div class="block block-login">
+          <h2 class="block-title">Login</h2>
+          <div class="block-content">
+            <form class="form-login" action="#">
+              <div class="form-item username">
+                <input type="text" name="username" class="text-input username" placeholder="Phone, email or username">
+              </div>
+              <div class="form-item password">
+                <input type="password" name="pasword" class="text-input pasword" placeholder="Password">
+              </div>
+              <div class="form-item rememberForgot">
+                <input type="checkbox" value="1" name="remember_me"> <span>Husk mig</span>
+              </div>
+              <input type="submit" class="submit btn primary-btn js-submit" value="Log in">
+            </form>
+          </div>
+        </div>
+
+        <div class="block block-content quote-large">
+          <div class="block-content">
+            <blockquote>Lorem ipsum dolor sit amet, ut sea error iriure, ipsum dolor usu at debet ipsum dolor mandamus</blockquote>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+
+  <div id="footer-wrapper">
+    <footer id="footer" class="region region-footer">
+      <div class="block quote-normal">
+        <div class="quote-item">
+          <blockquote>
+            ...Lorem ipsum dolor sit amet, ut sea error iriure, usu at debet mandamus. Pri debet dicit errem no, fugit nominati scripserit at mel. <br>
+            Commodo volumus cu pro. Mea cu everti causae qualisque, postea habemus vivendo qui ex. Nam ne suas decore indoctum, no mollis appareat rationibus his...”
+          </blockquote>
+          <cite>Inge Regneark, RevDut A/S</cite>
+        </div>
+        <div class="quote-item">
+          <blockquote>
+            ...Lorem ipsum dolor sit amet, ut sea error iriure, usu at debet mandamus. Pri debet dicit errem no, fugit nominati scripserit at mel. <br>
+            Commodo volumus cu pro. Mea cu everti causae qualisque, postea habemus vivendo qui ex. Nam ne suas decore indoctum, no mollis appareat rationibus his...”
+          </blockquote>
+          <cite>Inge Regneark, RevDut A/S</cite>
+        </div>
+      </div>
+    </footer>
+  </div>
+</body>
+</html>
